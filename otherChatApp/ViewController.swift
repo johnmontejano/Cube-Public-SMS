e4a035a4a427e101788188f60c26258071128040
johnmontewhat <us4john@gmail.com>
2016-07-13 16:52:35 -0700

    Initial Commit

---
+++ otherChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  otherChatApp
+//
+//  Created by John Montejano on 7/13/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
