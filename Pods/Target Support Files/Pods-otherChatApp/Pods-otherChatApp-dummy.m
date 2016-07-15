98a3c68540436d35b8cdbfefeb82fd2fa95aa6e0
johnmontewhat <us4john@gmail.com>
2016-07-15 10:37:19 -0700

    hey

---
+++ Pods/Target Support Files/Pods-otherChatApp/Pods-otherChatApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_otherChatApp : NSObject
+@end
+@implementation PodsDummy_Pods_otherChatApp
+@end
--- otherChatApp/ViewController.swift
+++ otherChatApp/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterChatButton: UIButton!
+    @IBOutlet weak var nickNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+                // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func enterChatButtonStart(sender: AnyObject) {
+        if nickNameTextField != ""{
+            self.performSegueWithIdentifier("startChatting", sender: self)
+        }
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let destination = segue.destinationViewController as? ChatViewController {
+            destination.senderId = UIDevice.currentDevice().identifierForVendor!.UUIDString
+            destination.senderDisplayName = nickNameTextField.text
+            destination.chatRooomName = "Public Room Chat"
+        }
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
-
